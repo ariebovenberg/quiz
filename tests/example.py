2fa673461f33a121dbea0dbdec3217232d644e83
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-08-11 19:04:37 +0200

    move example schema to separate file

---
+++ tests/example.py
@@ -0,0 +1,71 @@
+import typing as t
+from functools import partial
+
+from quiz import types
+from quiz.utils import FrozenDict
+
+mkfield = partial(types.FieldSchema,
+                  args=FrozenDict(),
+                  is_deprecated=False,
+                  desc='',
+                  deprecation_reason=None)
+
+
+Command = types.Enum('Command', {'SIT': 'SIT', 'DOWN': 'DOWN'})
+
+
+class Sentient(types.Interface):
+    name = mkfield('name', type=str)
+
+
+class Hobby(types.Object):
+    name = mkfield('name', type=str)
+    cool_factor = mkfield('description', type=int)
+
+
+class Human(Sentient, types.Object):
+    name = mkfield('name', type=str)
+    hobbies = mkfield('hobbies', type=t.Optional[t.List[t.Optional[Hobby]]])
+
+
+class Alien(Sentient, types.Object):
+    name = mkfield('name', type=str)
+    home_planet = mkfield('home_planer', type=t.Optional[str])
+
+
+class Dog(Sentient, types.Object):
+    """An example type"""
+    name = mkfield('name', type=str)
+    is_housetrained = mkfield(
+        'is_housetrained',
+        args=FrozenDict({
+            'at_other_homes': types.InputValue(
+                'at_other_homes',
+                '',
+                type=t.Optional[bool]
+            )
+        }),
+        type=bool)
+    bark_volume = mkfield('bark_volume', type=int)
+    knows_command = mkfield(
+        'knows_command',
+        args=FrozenDict({
+            'command': types.InputValue(
+                'command',
+                'the command',
+                type=Command
+            ),
+        }),
+        type=bool
+    )
+    owner = mkfield('owner', type=t.Optional[Human])
+    best_friend = mkfield('best_friend', type=t.Optional[Sentient])
+
+
+class Query(types.Object):
+    dog = mkfield('dog', type=Dog)
+
+
+HumanOrAlien = t.Union[Human, Alien]
+
+Human.best_friend = mkfield('best_friend', type=t.Optional[HumanOrAlien])
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,5 @@
 import enum
 import typing as t
-from functools import partial
 from textwrap import dedent
 
 import pytest
@@ -8,82 +7,8 @@ import pytest
 import quiz
 from quiz import schema, types
 from quiz.types import selector as _, Field, InlineFragment
-from quiz.utils import FrozenDict
 
-mkfield = partial(types.FieldSchema,
-                  args=FrozenDict(),
-                  is_deprecated=False,
-                  desc='',
-                  deprecation_reason=None)
-
-
-# TODO: move example schema to separate file?
-Command = types.Enum('Command', {'SIT': 'SIT', 'DOWN': 'DOWN'})
-
-
-class Sentient(types.Interface):
-    name = mkfield('name', type=str)
-
-
-class Hobby(types.Object):
-    name = mkfield('name', type=str)
-    cool_factor = mkfield('description', type=int)
-
-
-class Human(Sentient, types.Object):
-    name = mkfield('name', type=str)
-    hobbies = mkfield('hobbies', type=t.Optional[t.List[t.Optional[Hobby]]])
-
-
-class Alien (Sentient, types.Object):
-    name = mkfield('name', type=str)
-    home_planet = mkfield('home_planer', type=t.Optional[str])
-
-
-class Dog(Sentient, types.Object):
-    """An example type"""
-    name = mkfield('name', type=str)
-    is_housetrained = mkfield(
-        'is_housetrained',
-        args=FrozenDict({
-            'at_other_homes': types.InputValue(
-                'at_other_homes',
-                '',
-                type=t.Optional[bool]
-            )
-        }),
-        type=bool)
-    bark_volume = mkfield('bark_volume', type=int)
-    knows_command = mkfield(
-        'knows_command',
-        args=FrozenDict({
-            'command': types.InputValue(
-                'command',
-                'the command',
-                type=Command
-            ),
-        }),
-        type=bool
-    )
-    owner = mkfield('owner', type=t.Optional[Human])
-    best_friend = mkfield('best_friend', type=t.Optional[Sentient])
-
-
-class Query(types.Object):
-    dog = mkfield('dog', type=Dog)
-
-
-HumanOrAlien = t.Union[Human, Alien]
-
-Human.best_friend = mkfield('best_friend', type=t.Optional[HumanOrAlien])
-
-
-CLASSES = {
-    'Dog': Dog,
-    'Query': Query,
-    'Command': Command,
-    'Human': Human,
-}
+from .example import Dog, Query, Command, Hobby
 
 
 class TestEnumAsType:
